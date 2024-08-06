aad3c451cc32cb55c941fea5d0929aa810260ab0
Diogo Expedito <aspdiogo346@gmail.com>
2024-08-06 11:17:35 -0300

    atualizacao_sistema

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ caminho_1 = r"C:\Users\User\OneDrive\Trabalho\ESA 2024\Dashboard_ESA\base_dados\
 
 caminho_2 = r"C:\Users\User\OneDrive\Trabalho\ESA 2024\Dashboard_ESA\base_dados\Cel Manfrini(1).csv"
 
-#caminho_3 = r"C:\Users\User\OneDrive\Trabalho\ESA 2024\Dashboard_ESA\base_dados\Cel Manfrini(2).csv"
+caminho_3 = r"C:\Users\User\OneDrive\Trabalho\ESA 2024\Dashboard_ESA\base_dados\Cel Manfrini(2).csv"
 
 #caminho_4 = r"C:\Users\User\OneDrive\Trabalho\ESA 2024\Dashboard_ESA\base_dados\Cel Manfrini(3).csv"
 
@@ -76,17 +76,17 @@ caminho_2 = r"C:\Users\User\OneDrive\Trabalho\ESA 2024\Dashboard_ESA\base_dados\
 
 
 @st.cache_data
-def load_data(caminho_1, caminho_2):
+def load_data(caminho_1, caminho_2, caminho_3):
     # Leitura dos arquivos .CSV em DataFrames individuais
     df1 = pd.read_csv(caminho_1)
     df2 = pd.read_csv(caminho_2)
-    #df3 = pd.read_csv(caminho_3)
+    df3 = pd.read_csv(caminho_3)
     #df4 = pd.read_csv(caminho_4)
     #df5 = pd.read_csv(caminho_5)
     #df6 = pd.read_csv(caminho_6)
 
     # Concatenação dos DataFrames em um único DataFrame
-    df = pd.concat([df1, df2], ignore_index=True)
+    df = pd.concat([df1, df2, df3], ignore_index=True)
 
     # Excluindo linhas que contenham a string "Tela" em qualquer coluna
     df = df[~df.apply(lambda row: row.astype(str).str.contains('Tela')).any(axis=1)]
@@ -104,7 +104,7 @@ def load_data(caminho_1, caminho_2):
     df['DIAS'] = df['DIAS'].astype(int)
     return df
 
-df = load_data(caminho_1=caminho_1, caminho_2=caminho_2)
+df = load_data(caminho_1=caminho_1, caminho_2=caminho_2, caminho_3=caminho_3)
 
 # ------------------------------------------
 #       STREAMLIT
